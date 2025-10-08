dc03f0d8871c3bb03d8fcc9cbae6b491834416d4
Rutuja Ugale <rutujaaugale@gmail.com>
2025-10-07 19:37:22 -0700

    Add files via upload

--- slip2_1.c
+++ slip2_1.c
@@ -1,94 +1,92 @@
 #include<stdio.h>
 #include<stdlib.h>
-void main()
+int allocation[20][20],max[20][20],available[20],need[20][20],safe[10],s=0;
+int finish[10], work[10], cnt=0, flag=0, temp=0;
+int p,r,j,i,ch,ind,req[10];
+
+void check()
 {
-	int p[10],at[10],bt[10],wt[10],tat[10];
-	int i,j,sum=0,temp,n;
-	float avg;
-	printf("Enter the limit: ");
-	scanf("%d",&n);
-	for(i=0;i<n;i++)
+	temp=0;
+	s=0;
+	for(i=0;i<p;i++)
+	for(j=0;j<r;j++)
+	need[i][j]=max[i][j]-allocation[i][j];
+	printf("\n Need Table is:: \n");
+	for(i=0;i<p;i++)
 	{
-		printf("Enter the process number: ");
-		scanf("%d",&p[i]);
-		
-		printf("Enter the process arrival time: ");
-		scanf("%d",&at[i]);
-		
-		printf("Enter the process burst time: ");
-		scanf("%d",&bt[i]);
-	}	
-	for(i=0;i<n;i++)
-	for(j=i+1;j<n;j++)
+		for(j=0;j<r;j++)
+		printf("%d\t",need[i][j]);
+		printf("\n");
+	}
+	for(i=0;i<p;i++)
+	finish[i]=0;
+	for(i=0;i<r;i++)
+	work[i]=available[i];
+	while(temp<2)
 	{
-		if(at==0)
+		for(i=0;i<p;i++)
 		{
-			temp=p[0];
-			p[0]=p[i];
-			p[i]=temp;
-			
-			temp=at[0];
-			at[0]=at[i];
-			at[i]=temp;
-			
-			temp=bt[0];
-			bt[0]=bt[i];
-			bt[i]=temp;
+			for(j=0;j<r;j++)
+			{
+				if(finish[i]==0 && need[i][j] <= work[j])
+				flag=1;
+				else
+				{
+					flag=0;
+					break;
+				}
+				
+				if(flag==1)
+				{
+					for(j=0;j<r;j++)
+					work[j]=work[j]+allocation[i][j];
+					finish[i]=1;
+					safe[s++]=i;
+				}
+			}
+			temp ++;
+		}
+		flag=0;
+		for(i=0;i<p;i++)
+		{
+			if(finish[i]==0)
+			{
+				flag=1;
+				break;
+			}
+		}
+		if(flag==1)
+		{
+			printf("\n System is in deadlock stake ");
+		}
+		else
+		{
+			printf("\n System is in safe state ");
+			printf("\n Safe Sequence is: ");
+			for(i=0;i<p;i++)
+			printf("p%d\t",safe[i]);
 		}
 	}
-	for(i=0;i<n;i++)
-	for(j=i+1;j<n;j++)
-	{
-		if(bt[i]>bt[j])
-		{
-			temp=p[i];
-			p[i]=p[j];
-			p[j]=temp;
-			
-			temp=at[i];
-			at[i]=at[j];
-			at[j]=temp;
-			
-			temp=bt[i];
-			bt[i]=bt[j];
-			bt[j]=temp;
-		}
-	}
-	printf("After Sorting ");
-	for(i=0;i<n;i++)
-	{
-		printf("p\n%d\t%d\t%d",p[i],at[i],bt[i]);
-	}
-	wt[0]=0;
-	for(i=0;i<n;i++)
-	{
-		wt[i+1]=wt[i]+bt[i];
-		sum+=(wt[i]-at[i]);
-	}
-	wt[n]=wt[n-1]+bt[n-1];
-	printf("\n sum of waiting time is:%d",sum);
-	avg=(float)sum/(float)n;
-	printf("\n average of waiting time is: %f",avg);
+}
+
+int main()
+{
+	system("Clear");
+	printf("\n ************\t BANKERS ALGORITHM \t************");
+	printf("\n \n Enter the number of resourses and processes: ");
+	scanf("%d%d",&r,&p);
+	printf("\n Enter the allocation table: \n");
+	for(i=0;i<p;i++)
+	for(j=0;j<r;j++)
+	scanf("%d",& allocation[i][j]);
 	
-	sum=0;
-	for(i=0;i<n;i++)
-	{
-		tat[i]=(wt[i]-at[i]);
-		sum+=wt[i+1]-tat[i];
-	}
-	printf("\n sum of turnaround time is: %d",sum);
-	avg=(float)sum/(float)n;
-	printf("\n average of turnaround time is: %f",avg);
+	printf("\n Enter the max Table: \n");
+	for(i=0;i<p;i++)
+	for(j=0;j<r;j++)
+	scanf("%d",&max[i][j]);
+	printf("\n Enter the vector available: ");
+	for(i=0;i<p;i++)
+	scanf("%d",&available[i]);
+	check();
 	
-	printf("\n Gantt Chart");
-	printf("\n*****************\n");
-	for(i=0;i<n;i++)
-	{
-		printf("p%d\t",p[i]);
-	}
-	printf("\n*****************\n");
-	for(i=0;i<n;i++)
-	{
-		printf("%d\t",wt[i]);
-	}
 }
